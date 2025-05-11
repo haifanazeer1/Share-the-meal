43c100ca8b529adbaaedf92fb4bdd5776711ee09
Haifa <haifanazeer1@gmail.com>
2025-05-11 12:34:22 +0530

    errors fixed

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: DashboardPage(),
+      home: Home(),
       routes: {
         '/home': (context) => const Home(),
         '/sign-in': (context) => SigninScreen(),
